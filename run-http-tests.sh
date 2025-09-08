183c058a77d4f082d8a360e4686696f830156ffb
Severin Johannessen <sevjoh23@gmail.com>
2025-09-08 10:47:24 +0200

    Done 2

--- run-http-tests.sh
+++ run-http-tests.sh
@@ -1,9 +1,17 @@
 # run-http-tests.sh
 set -euo pipefail
-./gradlew bootRun --no-daemon & APP_PID=$!
-until curl -sf http://127.0.0.1:8080/ping >/dev/null; do sleep 1; done
 
+./gradlew bootRun --no-daemon & APP_PID=$!
+
+# 60s timeout
+for i in {1..60}; do
+  if curl -sf http://127.0.0.1:8080/ping >/dev/null; then break; fi
+  sleep 1
+done
+
+# Grab CLI and run
 curl -fL -o ijhttp.zip "https://jb.gg/ijhttp/latest"
 unzip -qo ijhttp.zip
-./ijhttp --env-file http-client.env.json --env local --report pollapp.http
+./ijhttp/ijhttp --env-file http-client.env.json --env local --report pollapp.http
+
 kill $APP_PID
